403ae5d1c798ada3637b02d3be58a6f20cd911b8
Lylinne <nikkinoaya@gmail.com>
2019-05-02 12:28:26 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<script>
+		if (!Date.now) {
+  			Date.now = function now() {
+    		return new Date().getTime();
+  };
+}
+	</script>
+
+</body>
+</html>
+
